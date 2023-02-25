64dd3f0e732ec3525bce2a8c7f50e154aa2b1a02
ALEX <brawlleonak@gmail.com>
2023-02-25 15:16:39 +0200

    1

--- 11.py
+++ 11.py
@@ -1,39 +1,51 @@
-# import random
-# class F:
-#     def __init__(self):
-#         self.__a = 100
-#         self.__b = 300
-#         self.sum = None
-#     def __method(self):
-#         c = random.randint(1,2)
-#         if c == 1:
-#             self.sum = self.__a * self.__b
-#         elif c == 2:
-#             self.sum = self.__b + self.__a
+# # import random
+# # class F:
+# #     def __init__(self):
+# #         self.__a = 100
+# #         self.__b = 300
+# #         self.sum = None
+# #     def __method(self):
+# #         c = random.randint(1,2)
+# #         if c == 1:
+# #             self.sum = self.__a * self.__b
+# #         elif c == 2:
+# #             self.sum = self.__b + self.__a
+# #
+# #     def printer(self):
+# #         print(self.__method())
+# #         return self.__method()
+# #
+# # gg = F()
+# # gg.printer()
+#
+# from random import randint
+# class F():
+#     def __init__(self, a, b):
+#         self.__a = a
+#         self.__b = b
+#
+#     def __in_method(self):
+#         self.operacion = randint(1, 2)
+#         if self.operacion == 1:
+#             return self.__a + self.__b
+#         else:
+#             return self.__a - self.__b
 #
 #     def printer(self):
-#         print(self.__method())
-#         return self.__method()
+#         print(self.__in_method())
 #
-# gg = F()
-# gg.printer()
+# ob = F(10,15)
+# ob.printer()
+#
 
-from random import randint
-class F():
-    def __init__(self, a, b):
-        self.__a = a
-        self.__b = b
+def gen(iter_lst):
+    yield iter_lst
 
-    def __in_method(self):
-        self.operacion = randint(1, 2)
-        if self.operacion == 1:
-            return self.__a + self.__b
-        else:
-            return self.__a - self.__b
-
-    def printer(self):
-        print(self.__in_method())
-
-ob = F(10,15)
-ob.printer()
+lst = [1, 2, 3]
+iter_lst = lst.__iter__()
+temp = gen(iter_lst).__next__()
+print(list(temp))
+# print(gen(iter_lst).__next__())
+# for i in gen(iter_lst).__iter__():
+#     print(i)
 
